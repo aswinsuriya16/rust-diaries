5f72ac95c8915b374eb88522931ff8673f2472e0
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-06-28 09:22:30 +0000

    Traits with generics

---
+++ src/adv/traitswithgenerics.rs
@@ -0,0 +1,42 @@
+use std::f32::consts::PI;
+
+trait Shape<T> {
+    fn area(&self) -> T;
+}
+
+struct Rectangle<T> {
+    length: T,
+    width: T,
+}
+
+struct Circle {
+    radius: f32,
+}
+
+impl<T: std::ops::Mul<Output = T> + Copy> Shape<T> for Rectangle<T> {
+    fn area(&self) -> T {
+        self.length * self.width
+    }
+}
+
+impl Shape<f32> for Circle {
+    fn area(&self) -> f32 {
+        PI * self.radius * self.radius
+    }
+}
+
+fn print_area<T,U>(s:T)
+where 
+    T : Shape<U>,
+    U : std::fmt::Display {
+        println!("{}",s.area());
+    } 
+fn main() {
+    let c = Circle { radius: 12.2 };
+    println!("Circle : {}", c.area());
+
+    let r = Rectangle { length: 10, width: 5 };
+    println!("Rectangle : {}", r.area());
+    print_area(c);
+    print_area(r);
+}
